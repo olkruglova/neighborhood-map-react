f03bf0ed2344d5a83dc90d883d5de897f26cfe14
olkruglova <olkruhlova@gmail.com>
2018-07-30 18:13:44 +0200

    first commit

---
+++ src/map.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpened: false };
+    }
+
+
+    render(){
+        
+        return (
+            <div>
+                map
+            </div>
+        );
+    }
+}
+
+export default Map;
