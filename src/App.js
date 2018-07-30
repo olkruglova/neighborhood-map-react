a1dceeb659ed5beb8ccd61b4664f6dc831539de0
olkruglova <olkruhlova@gmail.com>
2018-07-31 00:58:26 +0200

    Add markers

--- src/App.js
+++ src/App.js
@@ -14,16 +14,45 @@ export class MapContainer extends Component {
   render() {
     return (
       <Map 
-          google={this.props.google} 
-          zoom={14}
-          
+        google={this.props.google}
+        //defaultStyle={myStyle}
+        defaultCenter={{
+          lat: 52.406374,
+          lng: 16.925168100000064
+        }}
+        defaultZoom={13}
+        onClick={this.onMapClicked} 
+        //bounds={bounds}>
       >
-       <Marker onClick={this.onMarkerClick}
+        <Marker
+        title={'Park Cytadela'}
+        name={'SoMA'}
+        position={{lat: 52.42167420000001, lng: 16.936194}} />
+        <Marker
+        title={'Cathedral of Poznan'}
+        name={'SoMA'}
+        position={{lat: 52.4115064, lng: 16.948639800000024}} />
+        <Marker
+        title={'Rusalka Lake'}
+        name={'SoMA'}
+        position={{lat: 52.4265609, lng: 16.87878230000001}} />
+        <Marker
+        title={'Chocolate Museum'}
+        name={'SoMA'}
+        position={{lat: 52.4064838, lng: 16.932899799999973}} />
+        <Marker
+        title={'Gate of Poznan'}
+        name={'SoMA'}
+        position={{lat: 52.412412, lng: 16.951546000000008}} />
+
+
+        <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
       </Map>
     )
   }
 }
+
 
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyCPSWk9Gj32S52481URJOAI3kcqgx3KYzk')
