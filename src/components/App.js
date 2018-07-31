e926a387ffab3235894ffc943c47b038383ee00b
olkruglova <olkruhlova@gmail.com>
2018-08-01 00:08:53 +0200

    Add InfoWindow to the map

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import {
+  InfoWindow,
   Marker,
   Map,
   GoogleApiWrapper
@@ -8,6 +9,27 @@ import {
 
 
 class MapContainer extends React.Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+ 
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
   render() {
     return (
       <Map 
@@ -21,29 +43,38 @@ class MapContainer extends React.Component {
         onClick={this.onMapClicked} 
       >
         <Marker
-        title={'Park Cytadela'}
-        name={'SoMA'}
-        position={{lat: 52.42167420000001, lng: 16.936194}} />
+          title={'Park Cytadela'}
+          name={'SoMA'}
+          position={{lat: 52.42167420000001, lng: 16.936194}} />
         <Marker
-        title={'Cathedral of Poznan'}
-        name={'SoMA'}
-        position={{lat: 52.4115064, lng: 16.948639800000024}} />
+          title={'Cathedral of Poznan'}
+          name={'SoMA'}
+          position={{lat: 52.4115064, lng: 16.948639800000024}} />
         <Marker
-        title={'Rusalka Lake'}
-        name={'SoMA'}
-        position={{lat: 52.4265609, lng: 16.87878230000001}} />
+          title={'Rusalka Lake'}
+          name={'SoMA'}
+          position={{lat: 52.4265609, lng: 16.87878230000001}} />
         <Marker
-        title={'Chocolate Museum'}
-        name={'SoMA'}
-        position={{lat: 52.4064838, lng: 16.932899799999973}} />
+          title={'Chocolate Museum'}
+          name={'SoMA'}
+          position={{lat: 52.4064838, lng: 16.932899799999973}} />
         <Marker
-        title={'Gate of Poznan'}
-        name={'SoMA'}
-        position={{lat: 52.412412, lng: 16.951546000000008}} />
+          title={'Gate of Poznan'}
+          name={'SoMA'}
+          position={{lat: 52.412412, lng: 16.951546000000008}} />
 
 
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+
       </Map>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import MarkerClick from './components/InfoWindow.js';
 import Sidebar from './components/sidebar.js';
 
 ReactDOM.render(<App />, document.getElementById('map'));
