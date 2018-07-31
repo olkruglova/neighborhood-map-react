883fddabcc6e0784bfcecfe023d6792471d7e069
olkruglova <olkruhlova@gmail.com>
2018-07-31 13:13:36 +0200

    Create a Markers Component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Markers from './markers.js';
 
 ReactDOM.render(<App />, document.getElementById('map'));
 registerServiceWorker();
