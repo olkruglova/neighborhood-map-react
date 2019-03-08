75e2fb9691f4da946bd1fa49bd908cf0c7f30f54
olkruglova <olkruhlova@gmail.com>
2019-03-08 11:05:59 +0100

    Deploy site

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from '../../../../.cache/typescript/2.9/node_modules/@types/react';
-import ReactDOM from '../../../../.cache/typescript/2.9/node_modules/@types/react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
